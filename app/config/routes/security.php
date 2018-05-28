a18777f6edfb110be95afa26e87a01cfe10ba3d4
Arnaud BODEL <contact@osw3.net>
2018-05-28 11:34:27 +0200

    -

---
+++ app/config/routes/security.php
@@ -0,0 +1,8 @@
+<?php 
+$routes = [
+
+	["signup",  "/signup",  "Security:Security:signup",  "POST"],
+	["signin",  "/signin",  "Security:Security:signin",  "GET|POST"],
+	["logout",  "/logout",  "Security:Security:logout",  "GET"],
+    
+];
